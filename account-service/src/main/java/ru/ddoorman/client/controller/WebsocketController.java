d864cccdcb84d8efba820a88833ec79b06fe6c3d
sfede4kin <s.fede4kin@yandex.ru>
2022-09-25 13:09:40 +0300

    websocket controller, simple repository test with flyway and postgres testcontainers

---
+++ account-service/src/main/java/ru/ddoorman/client/controller/WebsocketController.java
@@ -0,0 +1,29 @@
+package ru.ddoorman.client.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import ru.ddoorman.client.model.dto.AccountDto;
+import ru.ddoorman.client.model.dto.DtoUtil;
+import ru.ddoorman.client.service.AccountService;
+
+@Controller
+public class WebsocketController {
+    private static final Logger log = LoggerFactory.getLogger(WebsocketController.class);
+    public final AccountService accountService;
+    public WebsocketController(AccountService accountService){
+        this.accountService = accountService;
+    }
+
+    @MessageMapping("/account.{accountId}")
+    @SendTo("/topic/response.{accountId}")
+    public AccountDto getAccount(@DestinationVariable String accountId) {
+        log.info("account id:{}", accountId);
+        var account = accountService.findById(Long.valueOf(accountId));
+        log.debug(account.get().toString());
+        return account.map(DtoUtil::cloneAccountToDto).get();
+    }
+}
---
+++ account-service/src/test/java/ru/ddoorman/client/test/AccountRepositoryTestContainersFlyway.java
@@ -0,0 +1,32 @@
+package ru.ddoorman.client.test;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import ru.ddoorman.client.repository.AccountRepository;
+import ru.ddoorman.client.service.AccountService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
+@ActiveProfiles("test-containers-flyway")
+public class AccountRepositoryTestContainersFlyway {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Test
+    public void shouldGetAccount(){
+        final var account = accountService.findById(1L);
+        assertTrue(account.isPresent());
+        assertEquals(account.get().getKeyGroups().size(), 2);
+    }
+
+}
